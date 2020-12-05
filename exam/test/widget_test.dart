a2b51263b91cffc4c9909fdf386aa61a34f2b1f4
Usamakhadim <74703163+Usamakhadim@users.noreply.github.com>
2020-12-05 14:36:20 +0500

    splash screen

--- exam/test/widget_test.dart
+++ exam/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:exam/main.dart'
+import 'package:flare_tutorial/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
